000e3ab72cfae2e3fe49450f0d5b4f75326afe7b
VamsiKrishna26 <39048968+VamsiKrishna26@users.noreply.github.com>
2022-02-09 18:22:52 +0000

    13th Commit

--- src/App.js
+++ src/App.js
@@ -17,8 +17,6 @@ import Skills from './pages/Skills/Skills';
 import styled from 'styled-components';
 
 const AppDiv=styled.div`
-@import url("https://fonts.googleapis.com/css2?family=Arvo:wght@700&family=Comforter&family=Dongle:wght@300&family=Inconsolata:wght@300&family=Josefin+Sans:ital,wght@1,300&family=Nunito+Sans:wght@300&family=Nunito:wght@300&family=Raleway:wght@200&family=Roboto:wght@300&family=Source+Sans+Pro:wght@300&display=swap");
-
 .App{
 
     .heading1 {
